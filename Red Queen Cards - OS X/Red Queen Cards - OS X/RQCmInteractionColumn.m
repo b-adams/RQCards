761bb5a08a566ef95f1936d9824e560573470e03
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-05-27 17:58:57 -0400

    Initial implementation of core board model

--- Red Queen Cards - OS X/Red Queen Cards - OS X/RQCmInteractionColumn.m
+++ Red Queen Cards - OS X/Red Queen Cards - OS X/RQCmInteractionColumn.m
@@ -8,28 +8,116 @@
 
 #import "RQCmInteractionColumn.h"
 
-@implementation RQCmInteractionColumn
+const int erSlots = 10;
 
-#pragma mark - RQC_MI_Columnar methods
+@implementation RQCmInteractionColumn
+{
+    NSUInteger _MAMP;
+    NSUInteger _PRR;
+    NSUInteger _Effectors[erSlots];
+    NSUInteger _RProteins[erSlots];
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _MAMP=0;
+        _PRR=0;
+        for(int i=0; i<erSlots; i+=1)
+        {
+            _Effectors[i]=0;
+            _RProteins[i]=0;
+        }
+    }
+    return self;
+}
+
+
+#pragma mark - conforming to RQC_MI_Columnar
 
 - (void)enableMAMP
 {
-
+    _MAMP += 1;
+    [self updateMAMPAlarm];
 }
 
 - (void)enablePRR
 {
-
+    _PRR += 1;
+    [self updateMAMPAlarm];
 }
 
 - (void)enableEffector:(NSUInteger)variant
 {
-
+    if([self checkLegalityOfVariant:variant])
+    {
+        _Effectors[variant] += 1;
+        [self updateMAMPAlarm]; //Possible disabling
+        [self updateEffectorAlarm]; 
+    }
 }
 
 - (void)enableRProtein:(NSUInteger)variant
 {
+    if([self checkLegalityOfVariant:variant])
+    {
+        _RProteins[variant] += 1;
+        [self updateEffectorAlarm];
+    }
+}
 
+#pragma mark - Alarm methods
+
+- (void)updateEffectorAlarm
+{
+    int matches=0;
+    for(int i=0; i<erSlots; i+=1)
+    {
+        if(_Effectors[i] && _RProteins[i]) matches+=1;
+    }
+    BOOL triggered = (matches>0);
+    if(triggered != [self isEffectorAlarmTriggered])
+        [self setIsEffectorAlarmTriggered:triggered];
+}
+
+- (void)updateMAMPAlarm
+{
+    BOOL disabled = NO;
+    for(int i=0; i<erSlots; i+=1)
+    {
+        if(_Effectors[i]) disabled = YES;
+    }
+    BOOL triggered = (_MAMP && _PRR && !disabled);
+    if(triggered != [self isMAMPAlarmTriggered])
+        [self setIsMAMPAlarmTriggered:triggered];
+}
+
+#pragma mark - Other helper methods
+
+-(BOOL) checkLegalityOfVariant:(NSUInteger) variant
+{
+    BOOL legal = variant<erSlots;
+
+    if(!legal)
+    {
+        NSException* ex = nil;
+        NSString* rsn = nil;
+        
+        rsn = [NSString stringWithFormat:@"Index %ld exceeds %d available slots",
+               (unsigned long)variant, erSlots];
+        ex = [NSException exceptionWithName:@"Out of Bounds"
+                                     reason:rsn
+                                   userInfo:nil];
+        @throw ex;
+    }
+
+    return legal;
+}
+-(void) reportBadVariant:(NSUInteger) variant
+{
+    NSLog(@"Warning: Attempting to access out-of-bounds variant %ld",
+          (unsigned long)variant);
 }
 
 @end
--- Red Queen Cards - OS X/Red Queen Cards - OS X/RQCmPlaymat.m
+++ Red Queen Cards - OS X/Red Queen Cards - OS X/RQCmPlaymat.m
@@ -6,31 +6,225 @@
 //  Copyright (c) 2013 Bryant Adams. All rights reserved.
 //
 
+#import "RQCmInteractionColumn.h"
 #import "RQCmPlaymat.h"
 
-@implementation RQCmPlaymat
+const NSUInteger MAMP_MATCHES_TO_TRIGGER_MTI = 2;
+const NSUInteger MAMP_COUNT_TO_TRIGGER_VIRULENCE = 2;
+const NSUInteger MAX_RPROTEINS_BEFORE_UNVIABILITY_RISK = 3;
+NSString *const KEY_ALARM_MAMP = @"alarmMAMP";
+NSString *const KEY_ALARM_EFFECTOR = @"alarmEffector";
 
-#pragma mark - RQC_MI_Playmatted methods
+@implementation RQCmPlaymat
+{
+    NSMutableDictionary* _columns;
+    NSUInteger _MAMPsMatched;
+    NSUInteger _MAMPsPlayed;
+    NSUInteger _RProteinsPlayed;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        [self setCurrentBoardState:RQC_e_Impotence];
+        _MAMPsMatched=0;
+        _MAMPsPlayed=0;
+        _RProteinsPlayed=0;
+        
+        _columns = [NSMutableDictionary dictionary];
+        
+    }
+    return self;
+}
+
+-(id<RQC_MI_Columnar>)getColumnNamed:(NSString*) name
+{
+    id<RQC_MI_Columnar> aColumn = nil;
+    aColumn = [_columns objectForKey:name];
+    if(!aColumn) aColumn = [self addColumnNamed:name];
+    return aColumn;
+}
+
+-(id<RQC_MI_Columnar>)addColumnNamed:(NSString*) name
+{
+    id aColumn = [[RQCmInteractionColumn alloc] init];
+    [aColumn addObserver:self
+              forKeyPath:KEY_ALARM_EFFECTOR
+                 options:NSKeyValueObservingOptionNew
+                 context:NULL];
+    [aColumn addObserver:self
+              forKeyPath:KEY_ALARM_MAMP
+                 options:NSKeyValueObservingOptionNew
+                 context:NULL];
+    [_columns setObject:aColumn
+                forKey:name];
+    return aColumn;
+}
+
+#pragma mark - conforming to RQC_MI_Playmatted
 
 - (void)playMAMP:(NSString *)colName
 {
+    id<RQC_MI_Columnar> theColumn = [self getColumnNamed:colName];
 
+    _MAMPsPlayed+=1; //Will need to be improved for duplicate MAMPs?
+    if(_MAMPsPlayed >= MAMP_COUNT_TO_TRIGGER_VIRULENCE)
+        [self triggerVirulence];
+    
+    [theColumn enableMAMP];
 }
 - (void)playMAMPDetector:(NSString *)colName
 {
-
+    id<RQC_MI_Columnar> theColumn = [self getColumnNamed:colName];
+    [theColumn enablePRR];
 }
 
 - (void)playEffector:(NSString *)colName
        variantNumber:(NSUInteger)variant
 {
-
+    id<RQC_MI_Columnar> theColumn = [self getColumnNamed:colName];
+    [theColumn enableEffector:variant];
 }
 
 - (void)playEffectorDetector:(NSString *)colName
                variantNumber:(NSUInteger)variant
 {
+    id<RQC_MI_Columnar> theColumn = [self getColumnNamed:colName];
 
+    _RProteinsPlayed+=1;
+    NSInteger riskLevel;
+    riskLevel = _RProteinsPlayed - MAX_RPROTEINS_BEFORE_UNVIABILITY_RISK;
+    if(riskLevel>0 && (rand()%10 < riskLevel))
+        [self triggerUnviability];
+
+    [theColumn enableRProtein:variant];
 }
+
+#pragma mark - Alarm listeners
+
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary *)change
+                      context:(void *)context
+{
+    if([keyPath isEqualToString:KEY_ALARM_MAMP])
+    {
+        NSNumber* newValue = [change objectForKey:@"new"];
+        [self onMAMPAlarmUpdateInColumn:object
+                                toState:[newValue boolValue]];
+    }
+    else if([keyPath isEqualToString:KEY_ALARM_EFFECTOR])
+    {
+        NSNumber* newValue = [change objectForKey:@"new"];
+        newValue = [change objectForKey:@"new"];
+        [self onEffectorAlarmUpdateInColumn:object
+                                    toState:[newValue boolValue]];
+    }
+    else
+    {
+        NSLog(@"Warning: Observed change in %p's %@ but did not handle it",
+              object, keyPath);
+    }
+}
+
+-(void)onMAMPAlarmUpdateInColumn:(id<RQC_MI_Columnar>)theColumn
+                         toState:(BOOL) alarmIsActive
+{
+    if(alarmIsActive)
+    {
+        _MAMPsMatched+=1;
+        if(_MAMPsMatched >= MAMP_MATCHES_TO_TRIGGER_MTI)
+        {
+            [self triggerMTI];
+        }
+    }
+    else
+    {
+        _MAMPsMatched-=1;
+        if(_MAMPsMatched < MAMP_MATCHES_TO_TRIGGER_MTI)
+            [self triggerLowMAMPDetection];
+    }
+}
+
+-(void)onEffectorAlarmUpdateInColumn:(id<RQC_MI_Columnar>)theColumn
+                             toState:(BOOL) alarmIsActive
+{
+    if(alarmIsActive)
+        [self triggerETI];
+    else
+        NSLog(@"WARNING: Attempt to UNtrigger ETI!?");
+}
+
+#pragma mark - Board state transitions
+
+//Note: No need to trigger impotence:
+//Virulence and MTI both imply potence, and MAMPs cannot be removed,
+//ETI and Unviability happens regardless of potence, and cannot be escaped
+-(void)triggerETI
+{
+    switch([self currentBoardState])
+    {
+        case RQC_e_Unviability: break;  //You're broken, sorry!
+        case RQC_e_ETI:         
+        case RQC_e_MTI:
+        case RQC_e_Virulence:
+        case RQC_e_Impotence:
+            [self setCurrentBoardState:RQC_e_ETI];
+    }
+}
+-(void)triggerMTI
+{
+    switch([self currentBoardState])
+    {
+        case RQC_e_Unviability: break;  //You're broken, sorry!
+        case RQC_e_ETI:         break;  //ETI trumps all other normal states
+        case RQC_e_MTI:
+        case RQC_e_Virulence:
+        case RQC_e_Impotence:
+            [self setCurrentBoardState:RQC_e_MTI];
+    }
+}
+-(void)triggerLowMAMPDetection
+{
+    switch([self currentBoardState])
+    {
+        case RQC_e_Unviability: break;  //You're broken, sorry!
+        case RQC_e_ETI:         break;  //ETI trumps all other normal states
+        case RQC_e_Virulence:   break;  //Less detection is still virulent
+        case RQC_e_Impotence:   break;  //Less detection does not help
+        case RQC_e_MTI:                 //Stand down alarms
+            [self setCurrentBoardState:RQC_e_Virulence];
+    }
+}
+
+-(void)triggerVirulence
+{
+    switch([self currentBoardState])
+    {
+        case RQC_e_Unviability: break;  //You're broken, sorry!
+        case RQC_e_ETI:         break;  //ETI trumps all other normal states
+        case RQC_e_Virulence:   
+        case RQC_e_MTI:
+        case RQC_e_Impotence:
+        default:
+            [self setCurrentBoardState:RQC_e_Virulence];
+    }
+}
+
+-(void)triggerUnviability
+{
+    switch([self currentBoardState])
+    {
+        case RQC_e_Unviability:
+        case RQC_e_ETI:                 //Even breaks ETI!
+        case RQC_e_Virulence:
+        case RQC_e_MTI:
+        case RQC_e_Impotence:
+        default:
+            [self setCurrentBoardState:RQC_e_Unviability];
+    }
+}
+
 
 @end
