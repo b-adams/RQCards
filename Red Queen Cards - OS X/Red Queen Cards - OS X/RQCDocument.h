477c071e1f809362ef3331b64f9d34a9f8deb36d
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-05-28 22:31:26 -0400

    Adding basic GUI

--- Red Queen Cards - OS X/Red Queen Cards - OS X/RQCDocument.h
+++ Red Queen Cards - OS X/Red Queen Cards - OS X/RQCDocument.h
@@ -8,6 +8,37 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface RQCDocument : NSDocument
+@interface RQCDocument : NSDocument <NSWindowDelegate>
+
+- (IBAction)pushedAMAMP:(id)sender;
+- (IBAction)pushedADetector:(id)sender;
+- (IBAction)pushedA1Effector:(id)sender;
+- (IBAction)pushedA2Effector:(id)sender;
+- (IBAction)pushedA1Detector:(id)sender;
+- (IBAction)pushedA2Detector:(id)sender;
+
+- (IBAction)pushedBMAMP:(id)sender;
+- (IBAction)pushedBDetector:(id)sender;
+- (IBAction)pushedB1Effector:(id)sender;
+- (IBAction)pushedB2Effector:(id)sender;
+- (IBAction)pushedB1Detector:(id)sender;
+- (IBAction)pushedB2Detector:(id)sender;
+
+- (IBAction)pushedCMAMP:(id)sender;
+- (IBAction)pushedCDetector:(id)sender;
+- (IBAction)pushedC1Effector:(id)sender;
+- (IBAction)pushedC2Effector:(id)sender;
+- (IBAction)pushedC1Detector:(id)sender;
+- (IBAction)pushedC2Detector:(id)sender;
+
+- (IBAction)pushedDMAMP:(id)sender;
+- (IBAction)pushedDDetector:(id)sender;
+- (IBAction)pushedD1Effector:(id)sender;
+- (IBAction)pushedD2Effector:(id)sender;
+- (IBAction)pushedD1Detector:(id)sender;
+- (IBAction)pushedD2Detector:(id)sender;
+
+@property (weak) IBOutlet NSTextField *infoLabel;
+
 
 @end
--- Red Queen Cards - OS X/Red Queen Cards - OS X/RQCDocument.m
+++ Red Queen Cards - OS X/Red Queen Cards - OS X/RQCDocument.m
@@ -7,13 +7,27 @@
 //
 
 #import "RQCDocument.h"
+#import "RQCmPlaymat.h"
 
 @implementation RQCDocument
+{
+    id<RQC_MI_Playmatted> _theMat;
+    NSMutableDictionary* _relevantHazards;
+    NSMutableDictionary* _relevantDetectors;
+}
 
 - (id)init
 {
     self = [super init];
     if (self) {
+        _relevantDetectors = [NSMutableDictionary dictionaryWithCapacity:8];
+        _relevantHazards = [NSMutableDictionary dictionaryWithCapacity:8];
+        _theMat = [[RQCmPlaymat alloc] init];
+        [(id)_theMat addObserver:self
+                     forKeyPath:@"currentBoardState"
+                        options:NSKeyValueObservingOptionNew
+                        context:NULL];
+        
         // Add your subclass-specific initialization here.
     }
     return self;
@@ -30,6 +44,8 @@
 {
     [super windowControllerDidLoadNib:aController];
     // Add any code here that needs to be executed once the windowController has loaded the document's window.
+    [self updateBoardStateLabel];
+
 }
 
 + (BOOL)autosavesInPlace
@@ -56,4 +72,172 @@
     return YES;
 }
 
+#pragma mark - conforming to NSWindowDelegate -
+
+-(void)windowWillClose:(NSNotification *)notification
+{
+    [self invalidate];
+}
+
+#pragma mark Helpers
+
+-(void) invalidate
+{
+    [_theMat invalidate];
+    [(id)_theMat removeObserver:self
+                     forKeyPath:@"currentBoardState"];//                        context:NULL];
+}
+
+#pragma mark - Model responses -
+
+#pragma mark Observation
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary *)change
+                      context:(void *)context
+{
+    if(object!=_theMat || ![keyPath isEqualToString:@"currentBoardState"])
+    {
+        NSLog(@"WARNING: %p observed change in %p's %@ but didn't care.",
+              self, object, keyPath);
+        return;
+    }
+
+    [self updateBoardStateLabel];
+}
+
+#pragma mark Helpers
+
+-(void)updateBoardStateLabel
+{
+    NSString* newLabel;
+    switch([_theMat currentBoardState])
+    {
+        case RQC_e_ETI:
+            newLabel = @"ETI";
+            break;
+        case RQC_e_Impotence:
+            newLabel = @"Pathogen not viable";
+            break;
+        case RQC_e_MTI:
+            newLabel = @"MTI";
+            break;
+        case RQC_e_Unviability:
+            newLabel = @"Plant not viable";
+            break;
+        case RQC_e_Virulence:
+            newLabel = @"Virulence";
+            break;
+    }
+    [[self infoLabel] setStringValue:newLabel];
+    
+}
+
+#pragma mark - View responses -
+
+#pragma mark Helpers
+
+-(void)pushHazard:(NSString*) colName
+      usingButton:(id) theButton
+{
+    [_relevantHazards setObject:theButton forKey:colName];
+
+    [_theMat playMAMP:colName];
+    
+    [[theButton cell] setBackgroundColor:[NSColor orangeColor]];
+    [theButton setBordered:NO];
+}
+
+-(void)pushDetector:(NSString*) colName
+      usingButton:(id) theButton
+{
+    [_relevantDetectors setObject:theButton forKey:colName];
+
+    [_theMat playMAMPDetector:colName];
+    
+    [[theButton cell] setBackgroundColor:[NSColor brownColor]];
+    [theButton setBordered:NO];
+}
+
+-(void)pushHazard:(NSString*) colName
+        ofVariety:(NSUInteger) variant
+      usingButton:(id) theButton
+{
+    NSString* fullName = [NSString stringWithFormat:@"%@%ld", colName, variant];
+    [_relevantHazards setObject:theButton forKey:fullName];
+
+    [_theMat playEffector:colName variantNumber:variant];
+    
+    [[theButton cell] setBackgroundColor:[NSColor redColor]];
+    [theButton setBordered:NO];
+}
+
+-(void)pushDetector:(NSString*) colName
+          ofVariety:(NSUInteger) variant
+        usingButton:(id) theButton
+{
+    NSString* fullName = [NSString stringWithFormat:@"%@%ld", colName, variant];
+    [_relevantDetectors setObject:theButton forKey:fullName];
+
+    [_theMat playEffectorDetector:colName variantNumber:variant];
+    
+    [[theButton cell] setBackgroundColor:[NSColor greenColor]];
+    [theButton setBordered:NO];
+}
+
+#pragma mark Actions
+
+- (IBAction)pushedAMAMP:(id)sender { [self pushHazard:@"a"
+                                          usingButton:sender]; }
+- (IBAction)pushedBMAMP:(id)sender { [self pushHazard:@"b"
+                                          usingButton:sender]; }
+- (IBAction)pushedCMAMP:(id)sender { [self pushHazard:@"c"
+                                          usingButton:sender]; }
+- (IBAction)pushedDMAMP:(id)sender { [self pushHazard:@"d"
+                                          usingButton:sender]; }
+
+- (IBAction)pushedADetector:(id)sender { [self pushDetector:@"a"
+                                                usingButton:sender]; }
+- (IBAction)pushedBDetector:(id)sender { [self pushDetector:@"b"
+                                                usingButton:sender]; }
+- (IBAction)pushedCDetector:(id)sender { [self pushDetector:@"c"
+                                                usingButton:sender]; }
+- (IBAction)pushedDDetector:(id)sender { [self pushDetector:@"d"
+                                                usingButton:sender]; }
+
+
+- (IBAction)pushedA1Effector:(id)sender { [self pushHazard:@"a" ofVariety:1
+                                               usingButton:sender]; }
+- (IBAction)pushedA2Effector:(id)sender { [self pushHazard:@"a" ofVariety:2
+                                               usingButton:sender]; }
+- (IBAction)pushedB1Effector:(id)sender { [self pushHazard:@"b" ofVariety:1
+                                               usingButton:sender]; }
+- (IBAction)pushedB2Effector:(id)sender { [self pushHazard:@"b" ofVariety:2
+                                               usingButton:sender]; }
+- (IBAction)pushedC1Effector:(id)sender { [self pushHazard:@"c" ofVariety:1
+                                               usingButton:sender]; }
+- (IBAction)pushedC2Effector:(id)sender { [self pushHazard:@"c" ofVariety:2
+                                               usingButton:sender]; }
+- (IBAction)pushedD1Effector:(id)sender { [self pushHazard:@"d" ofVariety:1
+                                               usingButton:sender]; }
+- (IBAction)pushedD2Effector:(id)sender { [self pushHazard:@"d" ofVariety:2
+                                               usingButton:sender]; }
+
+
+- (IBAction)pushedA1Detector:(id)sender { [self pushDetector:@"a" ofVariety:1
+                                                 usingButton:sender]; }
+- (IBAction)pushedA2Detector:(id)sender { [self pushDetector:@"a" ofVariety:2
+                                                 usingButton:sender]; }
+- (IBAction)pushedB1Detector:(id)sender { [self pushDetector:@"b" ofVariety:1
+                                                 usingButton:sender]; }
+- (IBAction)pushedB2Detector:(id)sender { [self pushDetector:@"a" ofVariety:2
+                                                 usingButton:sender]; }
+- (IBAction)pushedC1Detector:(id)sender { [self pushDetector:@"c" ofVariety:1
+                                                 usingButton:sender]; }
+- (IBAction)pushedC2Detector:(id)sender { [self pushDetector:@"c" ofVariety:2
+                                                 usingButton:sender]; }
+- (IBAction)pushedD1Detector:(id)sender { [self pushDetector:@"d" ofVariety:1
+                                                 usingButton:sender]; }
+- (IBAction)pushedD2Detector:(id)sender { [self pushDetector:@"d" ofVariety:2
+                                                 usingButton:sender]; }
 @end
