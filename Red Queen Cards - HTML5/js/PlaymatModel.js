94cc3fe6dff8a439dd9a144ecda24d12978a455e
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-06-01 16:17:06 -0400

    Re-indenting

--- Red Queen Cards - HTML5/js/PlaymatModel.js
+++ Red Queen Cards - HTML5/js/PlaymatModel.js
@@ -15,103 +15,103 @@ function PlayMat()
     return this;
 }
 
-    /* Data setup */
+/* Data setup */
 
 PlayMat.prototype.clearBoard = function()
+{
+    this._columns = [];
+    for(var i=0; i<NUMBER_OF_PLAYABLE_COLUMNS; i+=1)
     {
-        this._columns = [];
-        for(var i=0; i<NUMBER_OF_PLAYABLE_COLUMNS; i+=1)
-        {
-            this._columns[i] = {
-                _MAMP: false,
-                _PRR: false,
-                _Effectors: [false, false],
-                _RProteins: [false, false]
-            }
+        this._columns[i] = {
+            _MAMP: false,
+            _PRR: false,
+            _Effectors: [false, false],
+            _RProteins: [false, false]
         }
     }
+}
 
-    /* Inputs */
+/* Inputs */
 
 PlayMat.prototype.playCell     = function(colIndex)
+{
+    if(colIndex>=NUMBER_OF_PLAYABLE_COLUMNS) alert("Column "+colIndex+" too high");
+    var theColumn = this._columns[colIndex];
+
+    switch(type)
     {
-        if(colIndex>=NUMBER_OF_PLAYABLE_COLUMNS) alert("Column "+colIndex+" too high");
-        var theColumn = this._columns[colIndex];
-
-        switch(type)
-        {
-            case "Feature":
-                theColumn._MAMP = true;
-                break;
-            case "Detector":
-                theColumn._PRR = true;
-                break;
-            case "Effector":
-                if(arguments.length<3) alert("Effector variant not specified");
-                theColumn._Effectors[arguments[2]] = true;
-                break;
-            case "Alarm":
-                if(arguments.length<3) alert("Alarm variant not specified");
-                theColumn._RProteins[arguments[2]] = true;
-                break;
-            default:
-                alert("Unknown cell type: "+type);
-        }
+        case "Feature":
+            theColumn._MAMP = true;
+            break;
+        case "Detector":
+            theColumn._PRR = true;
+            break;
+        case "Effector":
+            if(arguments.length<3) alert("Effector variant not specified");
+            theColumn._Effectors[arguments[2]] = true;
+            break;
+        case "Alarm":
+            if(arguments.length<3) alert("Alarm variant not specified");
+            theColumn._RProteins[arguments[2]] = true;
+            break;
+        default:
+            alert("Unknown cell type: "+type);
     }
+}
 
-    /* Board state queries */
+/* Board state queries */
 
 PlayMat.prototype.isCellActive(type, colIndex)
+{
+    if(colIndex>=NUMBER_OF_PLAYABLE_COLUMNS) alert("Too many columns:" +colIndex);
+    var theColumn = this._columns[colIndex];
+
+    switch(type)
     {
-        if(colIndex>=NUMBER_OF_PLAYABLE_COLUMNS) alert("Too many columns:" +colIndex);
-        var theColumn = this._columns[colIndex];
-
-        switch(type)
-        {
-            case "Feature":
-                return theColumn._MAMP;
-                break;
-            case "Detector":
-                return theColumn._PRR;
-                break;
-            case "Effector":
-                if(arguments.length<3) alert("Effector variant not specified");
-                return theColumn._Effectors[arguments[2]];
-                break;
-            case "Alarm":
-                if(arguments.length<3) alert("Alarm variant not specified");
-                return theColumn._RProteins[arguments[2]];
-                break;
-            default:
-                alert("Unknown cell type: "+type);
-        }
+        case "Feature":
+            return theColumn._MAMP;
+            break;
+        case "Detector":
+            return theColumn._PRR;
+            break;
+        case "Effector":
+            if(arguments.length<3) alert("Effector variant not specified");
+            return theColumn._Effectors[arguments[2]];
+            break;
+        case "Alarm":
+            if(arguments.length<3) alert("Alarm variant not specified");
+            return theColumn._RProteins[arguments[2]];
+            break;
+        default:
+            alert("Unknown cell type: "+type);
     }
+}
 
-    /* Interaction state queries */
+/* Interaction state queries */
 
 PlayMat.prototype.isPlantETIActive = function()
+{
+    function columnActive(aColumn)
     {
-        function columnActive(aColumn)
-        {
-            return ((aColumn._Effectors[0] && aColumn._RProteins[0])
-                ||  (aColumn._Effectors[1] && aColumn._RProteins[1]));
-        }
-        return this._columns.some(columnActive);
+        return ((aColumn._Effectors[0] && aColumn._RProteins[0])
+            ||  (aColumn._Effectors[1] && aColumn._RProteins[1]));
     }
+    return this._columns.some(columnActive);
+}
 
 PlayMat.prototype.isPlantMTIActive = function()
+{
+    function columnActive(aColumn)
     {
-        function columnActive(aColumn)
-        {
-            var disabled = aColumn._Effectors[0] || aColumn._Effectors[1];
-            var triggered = aColumn._MAMP && aColumn._PRR;
-            return triggered && !disabled;
-        }
-        return this._columns.filter(columnActive).length >= MAMP_MATCHES_TO_TRIGGER_MTI;
+        var disabled = aColumn._Effectors[0] || aColumn._Effectors[1];
+        var triggered = aColumn._MAMP && aColumn._PRR;
+        return triggered && !disabled;
     }
+    return this._columns.filter(columnActive).length >= MAMP_MATCHES_TO_TRIGGER_MTI;
+}
 
 PlayMat.prototype.isPathogenVirulent = function()
-    {
-        return !(this.isPlantETIActive() || this.isPlantMTIActive());
-    }
+{
+    return !(this.isPlantETIActive() || this.isPlantMTIActive());
+}
 
