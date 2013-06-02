299cf0484da4617e35cbbac4d6351283d502dd7b
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-06-01 21:31:25 -0400

    Starting up ViewSync code

---
+++ Red Queen Cards - HTML5/js/ViewSyncer.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: badams
+ * Date: 6/1/13
+ * Time: 9:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(".board > #c1 > .feature").append("Farfenugen");
