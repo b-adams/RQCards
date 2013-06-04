4ff230d92eb84c4c0d87fffb3007f2aff53b0606
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-06-04 18:26:12 -0400

    Refining flip animation (needs to not have active cells end upside down), beginning randomization, shifted board from class to id

--- Red Queen Cards - HTML5/js/ViewSyncer.js
+++ Red Queen Cards - HTML5/js/ViewSyncer.js
@@ -9,9 +9,11 @@
 var theModel = new PlayMat();
 var boardState = "Uninitialized";
 
+//$("#wrap").append($.zc("#test>.ing>ul>(li>{!for:n:data!})*4", {data: [1,2,3,5]}));
+
 function getElement(type, colIndex)
 {
-    var selector = ".board > #c"+(colIndex+1)+" > .";
+    var selector = "#board > #c"+(colIndex+1)+" > .";
     switch(type)
     {
         case TYPE_ALARM:
@@ -55,8 +57,16 @@ function doPlay(theElement, type, colIndex)
     var wide = (arguments.length < 4);
 
     var active;
-    if(wide)  active = theModel.toggleCell(type, colIndex);
-    else      active = theModel.toggleCell(type, colIndex, arguments[3]);
+    if(wide)
+    {
+        active = theModel.toggleCell(type, colIndex);
+        if(!theElement) theElement = getElement(type, colIndex);
+    }
+    else
+    {
+        active = theModel.toggleCell(type, colIndex, arguments[3]);
+        if(!theElement) theElement = getElement(type, colIndex, arguments[3]);
+    }
 
     if(theModel.isPlantETIActive())         boardState="ETI";
     else if(theModel.isPlantMTIActive())    boardState="MTI";
@@ -64,20 +74,41 @@ function doPlay(theElement, type, colIndex)
     console.log("Setting board state to "+boardState);
     //theElement.append(":\n"+boardState);
 
+
     if(active)
     {
         theElement.css("border-style", "solid");
         theElement.css("border-width", "2px");
-        theElement.css("-webkit-animation", "rectify 2s");
+        theElement.css("-webkit-animation", "select 1s");
         theElement.css("opacity", "1");
     } else {
         theElement.css("border-style", "dashed");
         theElement.css("border-width", "1px");
-        theElement.css("-webkit-animation", "");
+        theElement.css("-webkit-animation", "deselect 1s");
+        theElement.css("-webkit-transform", "rotateX(180deg)");
         theElement.css("opacity", "0.5");
     }
     window.document.title = ("Current state: "+boardState);
 }
+
+function doRandomize(features, detectors, effectors, alarms)
+{
+    if(!features) features=4;
+    if(!detectors) detectors=4;
+    if(!effectors) effectors=4;
+    if(!alarms) alarms=4;
+
+    var randPick;
+    var pickedElement;
+    for(var n=0; n<features; n+=1)
+    {
+        randPick = Math.floor((Math.random()*8));
+        doPlay(0, TYPE_FEATURE, randPick);
+    }
+
+
+}
+
 $(document).ready(function(){
     boardState="Ready for input";
     for(var i=0; i<NUMBER_OF_PLAYABLE_COLUMNS; i+=1)
@@ -89,5 +120,8 @@ $(document).ready(function(){
         connectElement(TYPE_ALARM, i, 0);
         connectElement(TYPE_ALARM, i, 1);
     }
-    //$(".board > #c1 > .feature").click(function(){ alert("Clicky"); });
+    var randomButton = $("#Randomizer");
+    randomButton.click(function(){ doRandomize(4,4,4,4);});
+    //$("#board > #c1 > .feature").click(function(){ alert("Clicky"); });
 });
+
