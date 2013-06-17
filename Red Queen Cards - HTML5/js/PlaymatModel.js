bbf281e796f9eca90faa25d5c1a999c9f495dacb
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-06-17 17:28:33 -0400

    Placing under AGPL3 license

--- Red Queen Cards - HTML5/js/PlaymatModel.js
+++ Red Queen Cards - HTML5/js/PlaymatModel.js
@@ -3,7 +3,22 @@
  * User: badams
  * Date: 5/31/13
  * Time: 9:34 PM
- * To change this template use File | Settings | File Templates.
+
+ Data model for Red Queen playmat game mechanic
+ Copyright (C) 2013 Bryant Adams
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 
 //"use strict"; // http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
