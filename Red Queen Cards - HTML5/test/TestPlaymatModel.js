74ce694062fb8c30e0bb4476a1e690185a1c3422
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-06-30 19:48:25 -0400

    Adding JSTestDriver unit testing capacity

---
+++ Red Queen Cards - HTML5/test/TestPlaymatModel.js
@@ -0,0 +1,18 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: badams
+ * Date: 6/30/13
+ * Time: 5:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+TestCase("PlayMatModelTest", {
+    "test initial": function() {
+        var mat = new PlayMat();
+        assertEquals(false, mat.isCellActive(TYPE_FEATURE, 1));
+    },
+    "test toggle": function() {
+        var pmat = new PlayMat();
+        assertEquals(true, pmat.toggleCell(TYPE_FEATURE, 1));
+    }
+});
